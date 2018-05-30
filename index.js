e554135f2f8f6c8e710caa8cecd6c21cb7e3018c
Jesse Shaw <jdshaw1987@gmail.com>
2018-05-30 13:02:09 -0400

    Added Socket.io

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var socket = io()
 $(() => {
     $("#send").click(()=>{
         var message = {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -19,6 +21,11 @@ app.post('/messages', (req, res) => {
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log("A user connected");
+})
+
+var server = http.listen(3000, () => {
     console.log(`server is listening on port ${server.address().port}`)
+
 });
