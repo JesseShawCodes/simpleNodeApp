aabee71d50c6e5c7370f7bdb6ee2a7ffe73ab738
Jesse Shaw <jdshaw1987@gmail.com>
2018-05-30 12:54:38 -0400

    Updated POST request

---
+++ index.js
@@ -0,0 +1,31 @@
+$(() => {
+    $("#send").click(()=>{
+        var message = {
+            "name": $("#name").val(),
+            "message": $("#message").val()
+        }
+        postMessage(message)
+    })
+    getMessages()
+})
+
+function addMessages(message) {
+    $("#messages").append(`
+        <section class="message">
+        <h4>${message.name} </h4>
+        <p>${message.message}</p>
+        </section>
+    `)
+}
+
+function getMessages() {
+    $.get('http://localhost:3000/messages', (data) => {
+        data.forEach(addMessages);
+    })
+}
+
+function postMessage(message) {
+    console.log(message);
+    $.post("http://localhost:3000/messages", message)
+    .then(addMessages(message))
+}
